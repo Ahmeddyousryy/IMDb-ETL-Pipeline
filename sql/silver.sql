3f07bb2c477eb033c968ef18f419945fc5c637b2
Ahmed Mohamed <ahmedyousry520@gmail.com>
2026-05-15 01:43:17 +0300

    Create DDL scrip for new tables in the silver layer

--- sql/silver.sql
+++ sql/silver.sql
@@ -8,3 +8,29 @@ CREATE TABLE silver.tmdb_movies (
 	budget BIGINT
 );
 
+DROP TABLE IF EXISTS silver.imdb_genres;
+CREATE TABLE silver.imdb_genres (
+	genre_id INT,
+	genre VARCHAR(20)
+);
+
+
+DROP TABLE IF EXISTS silver.imdb_movie_genre;
+CREATE TABLE silver.imdb_movie_genre (
+	movie_id VARCHAR(50),
+	genre_id INT
+);
+
+DROP TABLE IF EXISTS silver.imdb_movie_titles;
+CREATE TABLE silver.imdb_movie_titles (
+	movie_id VARCHAR(50),
+	movie_name VARCHAR(70),
+	is_adult BOOLEAN,
+	runtime INT,
+	release_year INT
+);
+
+
+
+
+
